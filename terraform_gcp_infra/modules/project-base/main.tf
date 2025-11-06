f6720c128b4c89c0a409195d914de86f9edacba1
root <root@sjjeong.ftt.inc>
2025-11-06 15:53:36 +0900

    fix: project-base locals self reference

--- terraform_gcp_infra/modules/project-base/main.tf
+++ terraform_gcp_infra/modules/project-base/main.tf
@@ -11,8 +11,8 @@ locals {
   trimmed_folder_id = var.folder_id == null ? "" : trimspace(var.folder_id)
   trimmed_org_id    = var.org_id == null ? "" : trimspace(var.org_id)
 
-  parent_folder_id = trimmed_folder_id != "" ? trimmed_folder_id : null
-  parent_org_id    = trimmed_org_id != "" ? trimmed_org_id : null
+  parent_folder_id = local.trimmed_folder_id != "" ? local.trimmed_folder_id : null
+  parent_org_id    = local.trimmed_org_id != "" ? local.trimmed_org_id : null
 }
 
 # 0) 프로젝트 생성 (+ 폴더/결제 연결)
