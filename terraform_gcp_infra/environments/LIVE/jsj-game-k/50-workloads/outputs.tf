3bceb7dfc7f5f011bdb808a103042ef0d869de7d
root <root@sjjeong.ftt.inc>
2025-11-13 14:34:47 +0900

    chore: jsj-game-k 환경 제거

--- terraform_gcp_infra/environments/LIVE/jsj-game-k/50-workloads/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_groups" {
-  description = "Map of unmanaged instance group self links (Load Balancer에서 사용)"
-  value       = { for k, v in google_compute_instance_group.custom : k => v.self_link }
-}
