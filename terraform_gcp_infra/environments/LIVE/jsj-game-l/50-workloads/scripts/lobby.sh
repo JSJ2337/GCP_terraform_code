eefe77559a2bb7fd43de80f231fa22e8e244634e
root <root@sjjeong.ftt.inc>
2025-11-17 11:26:18 +0900

    chore: jsj-game-l 환경 제거

--- terraform_gcp_infra/environments/LIVE/jsj-game-l/50-workloads/scripts/lobby.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-apt-get update -y
-apt-get install -y nginx google-fluentd
-systemctl enable nginx
-systemctl start nginx
-systemctl enable google-fluentd
-systemctl start google-fluentd
--- terraform_gcp_infra/environments/LIVE/jsj-game-l/50-workloads/scripts/was.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-apt-get update -y
-apt-get install -y docker.io google-fluentd
-systemctl enable docker
-systemctl start docker
-systemctl enable google-fluentd
-systemctl start google-fluentd
