2d96c15dfead1142fab8c840ff1fdb1102c677e4
root <root@sjjeong.ftt.inc>
2025-11-27 16:48:17 +0900

    debug(workloads): zone_suffix 변환 디버깅용 output 추가
    processed_instances의 zone 값과 region_primary 값을
    확인하기 위한 임시 디버깅 output 추가

--- terraform_gcp_infra/environments/LIVE/gcp-gcby/50-workloads/outputs.tf
+++ terraform_gcp_infra/environments/LIVE/gcp-gcby/50-workloads/outputs.tf
@@ -2,3 +2,17 @@ output "instance_groups" {
   description = "Map of unmanaged instance group self links (Load Balancer에서 사용)"
   value       = { for k, v in google_compute_instance_group.custom : k => v.self_link }
 }
+
+# DEBUG: zone_suffix 변환 확인용
+output "debug_processed_instances_zones" {
+  description = "DEBUG: processed_instances의 zone 값 확인"
+  value = {
+    for name, cfg in local.processed_instances :
+    name => lookup(cfg, "zone", "NO_ZONE_SET")
+  }
+}
+
+output "debug_region_primary" {
+  description = "DEBUG: module.naming.region_primary 값 확인"
+  value       = module.naming.region_primary
+}
