ece2b847bec21aeab5fd6d9373dd2dd5421aa967
Infra_JSJ <wwsdfv@gmail.com>
2025-12-08 16:02:04 +0900

    feat: 70-loadbalancers에 instance_group_ids output 추가

--- terraform_gcp_infra/proj-default-templet/70-loadbalancers/gs/outputs.tf
+++ terraform_gcp_infra/proj-default-templet/70-loadbalancers/gs/outputs.tf
@@ -22,3 +22,8 @@ output "lb_type" {
   description = "로드 밸런서 타입"
   value       = module.load_balancer.lb_type
 }
+
+output "instance_group_ids" {
+  description = "생성된 Instance Group self-link 맵"
+  value       = { for k, v in google_compute_instance_group.lb_instance_group : k => v.self_link }
+}
