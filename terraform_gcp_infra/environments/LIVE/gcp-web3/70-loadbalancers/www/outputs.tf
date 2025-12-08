9c8b0e5e4943fe2fa80701ceaaa50ca4aa41cd66
Infra_JSJ <wwsdfv@gmail.com>
2025-12-08 15:57:59 +0900

    chore: 디버그 outputs 제거 및 .terraform.lock.hcl 복원
    - 70-loadbalancers/www/outputs.tf에서 debug_* outputs 제거
    - .terraform.lock.hcl 파일 복원

--- terraform_gcp_infra/environments/LIVE/gcp-web3/70-loadbalancers/www/outputs.tf
+++ terraform_gcp_infra/environments/LIVE/gcp-web3/70-loadbalancers/www/outputs.tf
@@ -22,24 +22,3 @@ output "lb_type" {
   description = "로드 밸런서 타입"
   value       = module.load_balancer.lb_type
 }
-
-# Debug outputs - 문제 파악 후 삭제
-output "debug_var_instance_groups" {
-  description = "[DEBUG] terragrunt에서 전달받은 var.instance_groups"
-  value       = var.instance_groups
-}
-
-output "debug_effective_vm_details_keys" {
-  description = "[DEBUG] effective_vm_details의 키 목록"
-  value       = keys(local.effective_vm_details)
-}
-
-output "debug_processed_instance_groups" {
-  description = "[DEBUG] 최종 처리된 instance groups"
-  value       = local.processed_instance_groups
-}
-
-output "debug_instance_group_ids" {
-  description = "[DEBUG] 생성된 Instance Group IDs"
-  value       = { for k, v in google_compute_instance_group.lb_instance_group : k => v.self_link }
-}
